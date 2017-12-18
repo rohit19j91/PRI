9e70598c1556171591127e68267444aad3baa80a
sadekone <sadekseridj@gmail.com>
2017-12-18 19:59:22 +0100

    [FIX BUG]: fix bug for seding code confirmation by mail

--- app/src/main/java/fr/epita/android/pri/Tools/ByteArrayDataSource.java
+++ app/src/main/java/fr/epita/android/pri/Tools/ByteArrayDataSource.java
@@ -4,19 +4,12 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.logging.Logger;
-
-import javax.sql.DataSource;
 
 /**
  * Created by sadekseridj on 25/11/2017.
  */
 
-public class ByteArrayDataSource implements DataSource {
+public class ByteArrayDataSource implements javax.activation.DataSource {
     private byte[] data;
     private String type;
 
@@ -52,48 +45,5 @@ public class ByteArrayDataSource implements DataSource {
         throw new IOException("Not Supported");
     }
 
-        @Override
-    public Connection getConnection() throws SQLException {
-        return null;
-    }
 
-    @Override
-    public Connection getConnection(String s, String s1) throws SQLException {
-        return null;
-    }
-
-    @Override
-    public <T> T unwrap(Class<T> aClass) throws SQLException {
-        return null;
-    }
-
-    @Override
-    public boolean isWrapperFor(Class<?> aClass) throws SQLException {
-        return false;
-    }
-
-    @Override
-    public PrintWriter getLogWriter() throws SQLException {
-        return null;
-    }
-
-    @Override
-    public void setLogWriter(PrintWriter printWriter) throws SQLException {
-
-    }
-
-    @Override
-    public void setLoginTimeout(int i) throws SQLException {
-
-    }
-
-    @Override
-    public int getLoginTimeout() throws SQLException {
-        return 0;
-    }
-
-    @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        return null;
-    }
 }
